c12aa386fef756d405d6933537163999f81c6fe7
Joel Paim <joellpaim@hotmail.com>
2022-12-20 17:31:34 -0300

    Novos testes

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,17 +1,23 @@
 import { test } from '@japa/runner'
 
 test.group('Cidades', () => {
+  test('Autenticação requerida', async ({ client }) => {
+    const response = await client.get('/api/cidade/1')
+
+    response.assertStatus(401)
+  })
   test('mostrar cidades', async ({ client }) => {
-    const response = await client.get('/api/cidades')
+    const response = await client
+      .get('/api/cidades?pageNumber=1&pageSize=2&sortDirection=asc&sortColumn=nom_cidade')
+      .basicAuth('joellpaim', '123456')
 
     response.assertStatus(200)
-    //response.assertBodyContains({ hello: 'world' })
   })
 
   test('mostrar cidade id 1', async ({ client }) => {
-    const response = await client.get('/api/cidade/1')
+    const response = await client.get('/api/cidade/1').basicAuth('joellpaim', '123456')
 
     response.assertStatus(200)
-    console.log(response.body())
+    //console.log(response.body())
   })
 })
